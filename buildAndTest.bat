08370083aad6b36b7e72d9b19cd741644b768549
Josh Chisholm <josh.chisholm@travcorpuk.com>
2015-10-07 14:59:01 +0100

    build script

---
+++ buildAndTest.bat
@@ -0,0 +1,1 @@
+powershell -ExecutionPolicy Bypass -File .\buildAndTest.ps1
---
+++ buildAndTest.ps1
@@ -0,0 +1,5 @@
+$msbuild = (Join-Path $env:winDir "Microsoft.NET\Framework64\v4.0.30319\MSBuild")
+
+& $msbuild .\TTC.Deployment.sln
+
+& .\packages\NUnit.Runners.2.6.4\tools\nunit-console.exe .\TTC.Deployment.Tests\bin\Release\TTC.Deployment.Tests.dll
