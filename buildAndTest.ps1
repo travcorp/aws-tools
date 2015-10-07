fd447bd91df87112015617923b7b36c46ba3b3db
Josh Chisholm <josh.chisholm@travcorpuk.com>
2015-10-07 15:52:09 +0100

    trap exit codes inside build script

--- buildAndTest.ps1
+++ buildAndTest.ps1
@@ -7,5 +7,11 @@ trap
 $msbuild = (Join-Path $env:winDir "Microsoft.NET\Framework64\v4.0.30319\MSBuild")
 
 & $msbuild .\TTC.Deployment.sln
+if (-not $?) {
+  throw "Failed to compile the solution"
+}
 
 & .\packages\NUnit.Runners.2.6.4\tools\nunit-console.exe .\TTC.Deployment.Tests\bin\Release\TTC.Deployment.Tests.dll
+if (-not $?) {
+  throw "NUnit failed"
+}
