abedc17797b5196d86011103ef96bb67de8f5c29
Josh Chisholm <josh.chisholm@travcorpuk.com>
2015-11-18 12:10:56 +0000

    delete stack before and after assume role tests

--- TTC.Deployment.Tests/AssumeRoleTest.cs
+++ TTC.Deployment.Tests/AssumeRoleTest.cs
@@ -23,26 +23,24 @@ namespace TTC.Deployment.Tests
         AmazonS3Client _s3Client;
         User _user;
         BasicAWSCredentials _userCredentials;
-        string _accessKeyId;
-        string _userName;
+        
         Role _role;
         string _roleName;
-        string _bucketName;
+
+        private readonly string _userName = "aws_tools_assume_role_test_user";
+        private readonly string _bucketName = "aws-tools-test-bucket-1";
 
         [SetUp]
         public void SetUp()
         {
-            _userName = "test_user_21";
             _roleName = "assume-role-" + DateTime.Now.ToFileTime();
-            _bucketName = "aws-tools-test-bucket-1";
 
             _awsConfiguration = new AwsConfiguration
             {
                 AwsEndpoint = RegionEndpoint.USWest2,
                 Credentials = new TestSuiteCredentials()
             };
-            DeletePreviousTestStack();
-
+            
             _iamClient = new AmazonIdentityManagementServiceClient(
                 new AmazonIdentityManagementServiceConfig
                 {
@@ -50,6 +48,10 @@ namespace TTC.Deployment.Tests
                     ProxyHost = _awsConfiguration.ProxyHost,
                     ProxyPort = _awsConfiguration.ProxyPort
                 });
+
+            _s3Client = new AmazonS3Client(new AmazonS3Config { RegionEndpoint = _awsConfiguration.AwsEndpoint });
+
+            DeletePreviousTestStack();
 
             _user = _iamClient.CreateUser(new CreateUserRequest
             {
@@ -84,13 +86,14 @@ namespace TTC.Deployment.Tests
                 }"
             });
 
-            var accessKey = _iamClient.CreateAccessKey(new CreateAccessKeyRequest
-            {
-                UserName = _user.UserName
-            }).AccessKey;
-
-            _accessKeyId = accessKey.AccessKeyId;
+            var accessKey = _iamClient.CreateAccessKey(new CreateAccessKeyRequest { UserName = _user.UserName }).AccessKey;
             _userCredentials = new BasicAWSCredentials(accessKey.AccessKeyId, accessKey.SecretAccessKey);
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            DeletePreviousTestStack();
         }
 
         [Test]
@@ -145,84 +148,98 @@ namespace TTC.Deployment.Tests
 
             _awsConfiguration.RoleName = _role.Arn;
             var deployer = new Deployer(_awsConfiguration);
-             deployer.CreateStack(new StackTemplate {
+            deployer.CreateStack(new StackTemplate {
        
                 StackName = "SimpleBucketTestStack",
                 TemplatePath = CloudFormationTemplates.Path("simple-s3-bucket-template.json"),
                 AssumedRoleARN = _role.Arn
             });
 
-            _s3Client = new AmazonS3Client(
-                new AmazonS3Config
-                {
-                    RegionEndpoint =_awsConfiguration.AwsEndpoint
-                });
-
             var s3Response = _s3Client.GetBucketLocation(_bucketName);
             Assert.AreEqual(s3Response.HttpStatusCode, HttpStatusCode.OK);
-        }
-
-        [TearDown]
-        public void TearDown()
-        {
-            DeleteRole("some_role_that_is_no_good_test");
-            var userPolicies = _iamClient.ListUserPolicies(new ListUserPoliciesRequest { UserName = _userName }).PolicyNames;
-            foreach (var policy in userPolicies)
-            {
-                _iamClient.DeleteUserPolicy(new DeleteUserPolicyRequest
-                {
-                    UserName = _userName,
-                    PolicyName = policy
-                });
-            }
-            _iamClient.DeleteAccessKey(new DeleteAccessKeyRequest
-            {
-                UserName = _userName,
-                AccessKeyId = _accessKeyId
-            });
-            _iamClient.DeleteUser(new DeleteUserRequest { UserName = _userName });
-
-            try
-            {
-                _s3Client.DeleteBucket(_bucketName);
-            } catch { }
         }
 
         private void DeleteRole(string roleName)
         {
             try
             {
-                var roleResponse = _iamClient.GetRole(new GetRoleRequest
-                {
-                    RoleName = roleName
-                });
+                _iamClient.GetRole(new GetRoleRequest {RoleName = roleName});
+            }
+            catch (NoSuchEntityException)
+            {
+                return;
+            }
 
-                if (roleResponse.Role == null) return;
+            var rolePoliciesResponse = _iamClient.ListRolePolicies(new ListRolePoliciesRequest { RoleName = roleName });
 
-                var rolePoliciesResponse = _iamClient.ListRolePolicies(new ListRolePoliciesRequest
-                {
-                    RoleName = roleName
-                });
-           
-                foreach(var p in rolePoliciesResponse.PolicyNames)
-                {
-                    _iamClient.DeleteRolePolicy(new DeleteRolePolicyRequest
-                    {
-                        PolicyName = p,
-                        RoleName = roleName
-                    });
-                } 
-            
-                _iamClient.DeleteRole(new DeleteRoleRequest
-                {
-                    RoleName = roleName
-                });
-            } catch { }
+            foreach (var p in rolePoliciesResponse.PolicyNames)
+            {
+                var request = new DeleteRolePolicyRequest { PolicyName = p, RoleName = roleName };
+                IgnoringNoSuchEntity(() => _iamClient.DeleteRolePolicy(request));
+            }
+
+            IgnoringNoSuchEntity(() => _iamClient.DeleteRole(new DeleteRoleRequest { RoleName = roleName }));
         }
 
         private void DeletePreviousTestStack()
         {
+            DeleteRole("some_role_that_is_no_good_test");
+            DeleteUser(_userName);
             StackHelper.DeleteStack(_awsConfiguration.AwsEndpoint, "SimpleBucketTestStack");
+            IgnoringNoSuchBucket(() => _s3Client.DeleteBucket(_bucketName));
+        }
+
+        private void DeleteUser(string userName)
+        {
+            try
+            {
+                var userPolicies =
+                    _iamClient.ListUserPolicies(new ListUserPoliciesRequest {UserName = userName}).PolicyNames;
+
+                foreach (var policy in userPolicies)
+                {
+                    var request = new DeleteUserPolicyRequest {UserName = userName, PolicyName = policy};
+                    IgnoringNoSuchEntity(() => { _iamClient.DeleteUserPolicy(request); });
+                }
+            }
+            catch (NoSuchEntityException)
+            {
+                return;
+            }
+
+            var keys = _iamClient.ListAccessKeys(new ListAccessKeysRequest { UserName = _userName });
+
+            foreach (var key in keys.AccessKeyMetadata)
+            {
+                var request = new DeleteAccessKeyRequest { UserName = userName, AccessKeyId = key.AccessKeyId };
+                IgnoringNoSuchEntity(() => { _iamClient.DeleteAccessKey(request); });
+            }
+
+            IgnoringNoSuchEntity(() => _iamClient.DeleteUser(new DeleteUserRequest { UserName = userName }));
+        }
+
+        private void IgnoringNoSuchEntity(Action action)
+        {
+            try
+            {
+                action();
+            }
+            catch (NoSuchEntityException)
+            {
+                Console.WriteLine("Ignoring no such entity...");
+            }
+        }
+
+        private void IgnoringNoSuchBucket(Action action)
+        {
+            try
+            {
+                action();
+            }
+            catch (AmazonS3Exception)
+            {
+                Console.WriteLine("Ignoring no such bucket...");
+            }
         }
 
         private string GetAWSAccountIdFromArn(User user)
